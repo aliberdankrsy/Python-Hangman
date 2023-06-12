70ff935041407c933745cad85102db71d6f785bc
Ali Berdan Karasoy <aliberdankrsy@outlook.com>
2023-06-12 13:10:21 +0300

    Hangman Game
    Python Hangman Game

---
+++ hang_man.py
@@ -0,0 +1,36 @@
+name = input("What is your name? :")
+print("Welcome " + name + " time to play hangman")
+
+secret_word = "hellopython"
+
+guess_string = ""
+
+lives = 10
+
+while lives > 0:
+	character_left = 0
+
+
+
+	for character in secret_word:
+		if character in guess_string:
+			print(character)
+		else:
+			print("_")
+			character_left += 1
+
+
+	if character_left == 0:
+		print("You won!")
+		break
+
+
+	guess = input("Guess a letter :")
+	guess_string += guess
+
+	if guess not in secret_word:
+		lives -= 1
+		print("Wrong guess!")
+		print(f"You have {lives} left")
+		if lives == 0:
+			print("You died!")
